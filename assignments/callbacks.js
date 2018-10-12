9ea10d40daad977741d64147c4d1a77d16f658ae
John O'Keefe <jokeefe@weegolobal.com>
2018-10-12 01:17:06 -0400

    assignment callbacks complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,32 +21,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+    // getLength passes the length of the array into the callback.
+    return cb(arr.length);
 }
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+    // last passes the last item of the array into the callback.
+    return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+    // sumNums adds two numbers (x, y) and passes the result to the callback.
+    let result = x + y;
+    return cb(result);
 }
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+    // multiplyNums multiplies two numbers and passes the result to the callback.
+    let result = x * y;
+    return cb(result);
 }
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
+    let result = list.includes(item);
+    return cb(result);
 }
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+    // removeDuplicates removes all duplicate values from the given array.
+    // Pass the duplicate free array to the callback function.
+    // Do not mutate the original array.
+    let removeDuplicates = new Set(array);
+    let result = Array.from(removeDuplicates);
+
+    return cb(result);
 }
